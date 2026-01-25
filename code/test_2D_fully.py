96712c2746a5cbf251557f83daf6c0f07653fc95
lenomi <nminh.main@gmail.com>
2026-01-25 19:49:11 +0700

    fix file test

--- code/test_2D_fully.py
+++ code/test_2D_fully.py
@@ -12,13 +12,11 @@ from tqdm import tqdm
 from networks.net_factory import net_factory
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--root_path", type=str, default="../data/ACDC")
-parser.add_argument(
-    "--exp", type=str, default="ACDC/Uncertainty_Rectified_Pyramid_Consistency"
-)
-parser.add_argument("--model", type=str, default="unet_urpc")
-parser.add_argument("--num_classes", type=int, default=4)
-parser.add_argument("--labeled_num", type=int, default=7)
+parser.add_argument('--root_path', type=str, default='../data/ACDC')
+parser.add_argument('--exp', type=str, default='ACDC/Uncertainty_Rectified_Pyramid_Consistency')
+parser.add_argument('--model', type=str, default='unet_urpc')
+parser.add_argument('--num_classes', type=int, default=4)
+parser.add_argument('--labeled_num', type=int, default=7)
 
 
 def calculate_metric_percase(pred, gt):
@@ -33,9 +31,9 @@ def calculate_metric_percase(pred, gt):
 
 
 def test_single_volume(case, net, test_save_path, FLAGS):
-    h5f = h5py.File(FLAGS.root_path + "/data/{}.h5".format(case), "r")
-    image = h5f["image"][:]
-    label = h5f["label"][:]
+    h5f = h5py.File(FLAGS.root_path + "/data/{}.h5".format(case), 'r')
+    image = h5f['image'][:]
+    label = h5f['label'][:]
     prediction = np.zeros_like(label)
 
     net.eval()
@@ -48,7 +46,7 @@ def test_single_volume(case, net, test_save_path, FLAGS):
 
             out = net(input)
             if isinstance(out, (tuple, list)):
-                out = out[0]  # URPC: lấy output chính
+                out = out[0]   # URPC: lấy output chính
 
             out = torch.softmax(out, dim=1)
             out = torch.argmax(out, dim=1).squeeze(0).cpu().numpy()
@@ -74,26 +72,20 @@ def test_single_volume(case, net, test_save_path, FLAGS):
 
 
 def Inference(FLAGS):
-    with open(FLAGS.root_path + "/test.list", "r") as f:
+    with open(FLAGS.root_path + '/test.list', 'r') as f:
         image_list = f.readlines()
-    image_list = sorted([item.replace("\n", "").split(".")[0] for item in image_list])
+    image_list = sorted([item.replace('\n', '').split(".")[0] for item in image_list])
 
     snapshot_path = "../model/{}_{}_labeled/{}".format(
-        FLAGS.exp, FLAGS.labeled_num, FLAGS.model
-    )
+        FLAGS.exp, FLAGS.labeled_num, FLAGS.model)
     test_save_path = "../model/{}_{}_labeled/{}_predictions/".format(
-        FLAGS.exp, FLAGS.labeled_num, FLAGS.model
-    )
+        FLAGS.exp, FLAGS.labeled_num, FLAGS.model)
     if os.path.exists(test_save_path):
         shutil.rmtree(test_save_path)
     os.makedirs(test_save_path)
 
-    net = net_factory(
-        net_type=FLAGS.model, in_chns=1, class_num=FLAGS.num_classes
-    ).cuda()
-    save_mode_path = os.path.join(
-        snapshot_path, "{}_best_model.pth".format(FLAGS.model)
-    )
+    net = net_factory(net_type=FLAGS.model, in_chns=1, class_num=FLAGS.num_classes).cuda()
+    save_mode_path = os.path.join(snapshot_path, '{}_best_model.pth'.format(FLAGS.model))
     net.load_state_dict(torch.load(save_mode_path))
     print("init weight from {}".format(save_mode_path))
 
@@ -106,7 +98,7 @@ def Inference(FLAGS):
     return avg_metric
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     FLAGS = parser.parse_args()
     metric = Inference(FLAGS)
     print(metric)
